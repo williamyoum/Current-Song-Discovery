43a4c0cb802cf0f2d7ff64b2097721ccce49e456
Will <will.youm@gmail.com>
2019-10-11 02:21:51 -0400

    Updates

---
+++ precache-manifest.96e968a018c6dde29865986c0129e2fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5953fb2ac67defaea99d32886ab735",
+    "url": "/Current-Song-Discovery/index.html"
+  },
+  {
+    "revision": "07301e5ebf101f8af7bf",
+    "url": "/Current-Song-Discovery/static/css/main.9b7071e1.chunk.css"
+  },
+  {
+    "revision": "6e44cca819770c6b8985",
+    "url": "/Current-Song-Discovery/static/js/2.3a498c7d.chunk.js"
+  },
+  {
+    "revision": "07301e5ebf101f8af7bf",
+    "url": "/Current-Song-Discovery/static/js/main.ebdf4df2.chunk.js"
+  },
+  {
+    "revision": "864e452c9e3f53650d7e",
+    "url": "/Current-Song-Discovery/static/js/runtime-main.ffec89ed.js"
+  }
+]);
